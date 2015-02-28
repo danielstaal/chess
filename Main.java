58f3e64146814065151095c8491e271bb697c24b
danielstaal <daniel.staal@hotmail.com>
2015-02-28 16:11:04 +0100

    first

---
+++ Main.java
@@ -0,0 +1,50 @@
+/*
+ * File: Breakout.java
+ * -------------------
+ * Name: Daniel Staal
+ * 
+ * This file will eventually implement the game of Tanks.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class Main
+{
+	public void run()
+	{
+		printChessBoard();
+	}
+	
+	private void printChessBoard()
+	{
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Makefile
@@ -0,0 +1,20 @@
+CLASSPATH = acm.jar
+CFLAGS = -cp .:$(CLASSPATH)
+
+JAVA_FILES = Main.java
+CLASS_FILES = $(JAVA_FILES:.java=.class)
+
+all: Main
+
+Main: $(CLASS_FILES)
+
+%.class: %.java
+	javac $(CFLAGS) $<
+
+clean:
+	rm -f *.class
+
+submit: pset4.zip
+
+pset4.zip: $(JAVA_FILES)
+	zip $@ $^
